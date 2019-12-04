5988ec4f501ece4a1f420f660c5ee7409fc8a6cc
John R. Stokka <jrstokka@gmail.com>
2019-12-04 14:23:36 -0600

    Expanded Tick Record to include Bid/Ask

--- NCDFile.cs
+++ NCDFile.cs
@@ -63,38 +63,36 @@ namespace RespondClient.DomiKnow.NinjaTrader
     [Flags]
     public enum TickMask1Flags
     {
-        NoTime = 0,
-        Time1 = 1,
-        Time2 = 2,                  // unknown but assume could be used
-        Time4 = 3,
-        Time8 = 4,
-        Time1Secs = 5,
-        UnknownOption00001000 = 8,  // unknown but assume could be used
-        UnknownOption00010000 = 16, // unknown but assume could be used
-        UnknownOption00011000 = 24, // unknown but assume could be used
-        UnknownOption00100000 = 32, // unknown but assume could be used
-        UnknownOption00101000 = 33, // unknown but assume could be used
-        UnknownOption00110000 = 48, // unknown but assume could be used
-        UnknownOption00111000 = 56, // unknown but assume could be used
-        PriceEmbeddedInMask2 = 64,
-        Price1 = 128,
-        Price4 = 192                 
+        NoTime = 0,                     // x.....000    
+        Time1 = 1,                      // x.....001
+        Time2 = 2,                      // x.....010                          
+        Time4 = 3,                      // x.....011
+        Time8 = 4,                      // x.....100
+        Time1Secs = 5,                  // x.....101
+        Ask = 8,                        // x..xx1...  
+        DefaultSpreadX2 = 16,           // x..x1x...  
+        DefaultSpreadX3 = 32,           // x..1xx...  
+        Spread1 = 48,                   // x..110...  
+        Spread2 = 56,                   // x..111...
+        PriceEmbeddedInMask2 = 64,      // x01......
+        Price1 = 128,                   // x10......
+        Price4 = 192                    // x11......
     }
 
     [Flags]
     public enum TickMask2Flags
     {
-        PriceDeltaBit1 = 1,               // x....0001
-        PriceDeltaBit2 = 2,               // x....0010
-        PriceDeltaBit3 = 4,               // x....0100
-        PriceDeltaBit4 = 8,               // x....1000
-        Volume1 = 32,                     // x001.....
-        Volume1X100 = 64,                 // x010.....
-        Volume1X500 = 96,                 // x011..... 
-        Volume1X1000=128,                 // x100.....
-        Volume2 = 160,                    // x101.....
-        Volume4 = 192,                    // x110.....
-        Volume8 = 224                     // x111.....
+        PriceDeltaBit1 = 1,             // x....0001
+        PriceDeltaBit2 = 2,             // x....0010
+        PriceDeltaBit3 = 4,             // x....0100
+        PriceDeltaBit4 = 8,             // x....1000
+        Volume1 = 32,                   // x001.....
+        Volume1X100 = 64,               // x010.....
+        Volume1X500 = 96,               // x011..... 
+        Volume1X1000=128,               // x100.....
+        Volume2 = 160,                  // x101.....
+        Volume4 = 192,                  // x110.....
+        Volume8 = 224                   // x111.....
     }
 
     [Flags]
@@ -156,11 +154,15 @@ namespace RespondClient.DomiKnow.NinjaTrader
     {
         public DateTime DateTime;
         public double Price;
+        public double Bid;
+        public double Ask;
         public long Volume;
-        public TickRecord(DateTime dt, double price, long volume)
+        public TickRecord(DateTime dt, double price, double bid, double ask, long volume)
         {
             DateTime = dt;
             Price = price;
+            Bid = bid;
+            Ask = ask;
             Volume = volume;
         }
     }
@@ -245,7 +247,9 @@ namespace RespondClient.DomiKnow.NinjaTrader
     public class NCDTickFile : NCDFile<TickRecord>
     {
         public NCDTickFile(string path) : base(NCDFileType.Tick, path)
-        { }
+        {
+            _defaultSpread = _tickSizePrice;
+        }
         public override INCDRecord ReadNextRecord()
         {
             return ReadNextTickRecord();
@@ -253,6 +257,9 @@ namespace RespondClient.DomiKnow.NinjaTrader
 
         TickMask1Flags _lastMask1;
         TickMask2Flags _lastMask2;
+        Double _lastBid;
+        Double _lastAsk;
+        Double _defaultSpread;
         private TickRecord ReadNextTickRecord()
         {
             TickMask1Flags mask1 = (TickMask1Flags)_br.ReadByte();
@@ -267,6 +274,8 @@ namespace RespondClient.DomiKnow.NinjaTrader
             // Outputs
             DateTime barDateTime;
             Double barPrice;
+            Double barBid;
+            Double barAsk;
             long barVolume;
 
             //
@@ -377,8 +386,64 @@ namespace RespondClient.DomiKnow.NinjaTrader
                     $"FileName: {FileName}\n" +
                     $"Position: {_br.BaseStream.Position}");
             }
-
             barPrice = Math.Round(_lastPrice + ((int)(priceDelta - priceDeltaOffset) * _tickSizePrice), 2);
+
+            //
+            // check the Bid/Ask flag
+            //
+            int spreadMultiplier;
+            bool bidTrade;
+
+            if (mask1.HasOption(TickMask1Flags.Spread2))
+            {
+                spreadMultiplier = _br.ReadUInt16BE();
+                if (spreadMultiplier < 256)
+                {
+                    bidTrade = true;
+                }
+                else
+                {
+                    bidTrade = false;
+                    spreadMultiplier /= 256;
+                }
+            }
+            else if (mask1.HasOption(TickMask1Flags.Spread1))
+            {
+                spreadMultiplier = _br.ReadByte();
+                if (spreadMultiplier < 16)
+                {
+                    bidTrade = true;
+                }
+                else
+                {
+                    bidTrade = false;
+                    spreadMultiplier /= 16;
+                }
+            }
+            else
+            {
+                if (mask1.HasOption(TickMask1Flags.DefaultSpreadX3))
+                    spreadMultiplier = 3;
+                else if (mask1.HasOption(TickMask1Flags.DefaultSpreadX2))
+                    spreadMultiplier = 2;
+                else
+                    spreadMultiplier = 1;
+
+                if (mask1.HasOption(TickMask1Flags.Ask))
+                    bidTrade = false;
+                else
+                    bidTrade = true;
+            }
+            if (bidTrade)
+            {
+                barBid = barPrice;
+                barAsk = barBid + (spreadMultiplier * _defaultSpread);
+            }
+            else
+            {
+                barAsk = barPrice;
+                barBid = barAsk - (spreadMultiplier * _defaultSpread);
+            }
 
             //
             // check the Volume flag
@@ -416,11 +481,13 @@ namespace RespondClient.DomiKnow.NinjaTrader
 
             _lastDateTime = barDateTime;
             _lastPrice = barPrice;
+            _lastBid = barBid;
+            _lastAsk = barAsk;
             _lastVolume = barVolume;
             _lastMask1 = mask1;
             _lastMask2 = mask2;
 
-            return new TickRecord(barDateTime, barPrice, barVolume);
+            return new TickRecord(barDateTime, barPrice, barBid, barAsk, barVolume);
         }
 
     }
